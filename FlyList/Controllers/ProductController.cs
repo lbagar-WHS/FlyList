7c2ed43b1e91fac91da425b661a57330570b84eb
marco-rbk <marco.ribbekamp@gmail.com>
2024-05-13 14:42:48 +0200

    controller branch initital commit

---
+++ FlyList/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using FlyList.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlyList.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public List<Product> GetAllProducts()
+        {
+            return new List<Product>();
+        }
+
+        [HttpPost]
+        public IActionResult AddProduct([FromBody]Product newProduct)
+        {
+            return Ok("");
+        }
+
+        [HttpPut]
+        public IActionResult ModifyProduct([FromBody] Product product)
+        {
+            return Ok("");
+        }
+
+    }
+}
